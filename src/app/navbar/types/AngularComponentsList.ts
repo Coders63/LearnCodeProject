724e6d7dd5d97ec46e50c7f3700754df369374a9
poojahb-28 <poojahb98@gmail.com>
2023-04-03 22:05:03 +0530

    spelling mistake corrected

--- src/app/navbar/types/AngularComponentsList.ts
+++ src/app/navbar/types/AngularComponentsList.ts
@@ -5,9 +5,9 @@ import {Angularheader} from './headers'
 export const ComponentList : Angularheader[] =
    [
     {Id : 1, name :"Angular Home"},
-    {Id : 2,name : "Angular Inroudction"},
+    {Id : 2,name : "Angular Introduction"},
     {Id :3 , name : "Angular life cycle hooks"},
-    {Id : 4, name : "Angular Directivs"}
+    {Id : 4, name : "Angular Directives"}
 ];
 
 
